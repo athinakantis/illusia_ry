d507c3a72c16ae7406ace3122f8ba1ed350f1a9e
Athina <athina.kantis@gmail.com>
2025-03-27 19:51:55 +0200

    Add RouterProvider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import { Box } from '@mui/material';
 import { RouterProvider } from 'react-router-dom';
+import { router } from './router/router';
 import './App.css';
+import { NavBar } from './components/NavBar';
 
 function App() {
   return (
