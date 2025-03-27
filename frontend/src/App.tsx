29d6f48f68f3e930a5f568fec51dc6054ef96cca
Athina <athina.kantis@gmail.com>
2025-03-27 11:57:19 +0200

    Add browserRouter, RouteProvider and Home route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import { router } from './router/router';
 
 function App() {
-
   return (
-    <h1>Illusia Ry Storage Solutions</h1>
+    <RouterProvider router={router} />
   );
 }
 
