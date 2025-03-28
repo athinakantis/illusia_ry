4a6ebe477868bf598fdeb4d8e2661c79f3a3dced
Jonathan Gian <153126165+JonathanGian@users.noreply.github.com>
2025-03-28 14:10:03 +0200

    Updated testing for the backend and setup some e2e testing and spec testing. Check the README for how to run the tests and check the endpoints yourselves.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,12 +4,14 @@ import { AppController } from './controllers/app.controller';
 import { AppService } from './services/app.service';
 import { TestController } from './controllers/test.controller';
 import { SupabaseService } from './services/supabase.service';
+import { TestRoleController } from './controllers/test-role.controller';
+import { SupabaseRoleService } from './services/supabase-role.service';
 
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true })],
   
-  controllers: [AppController, TestController],// Controller imports go here
-  providers: [AppService, SupabaseService],// Services are used to handle business logic and data access
+  controllers: [AppController, TestController,TestRoleController],// Controller imports go here
+  providers: [AppService, SupabaseService,SupabaseRoleService],// Services are used to handle business logic and data access
 })
 export class AppModule {}
