db6327221a7404de6e91a4e3ea4150965bfa6630
Andrei Savolainen <andrei.savolainen@gmail.com>
2025-03-31 18:56:30 +0300

    Use useSelector to access the auth state and useDispatch to handle logout.

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -6,12 +6,15 @@ import {
   Toolbar,
   Typography,
   useTheme,
+  Button,
 } from '@mui/material';
 
-import { useContext} from 'react';
+import { useContext } from 'react';
 import { ThemeContext } from '../context/ThemeContext';
 import { lightPalette, darkPalette } from '../theme/theme';
 import { FiMoon, FiSun } from 'react-icons/fi';
+import { useSelector, useDispatch } from 'react-redux';
+import { logout } from '../store/authSlice';
 
 export const NavBar = () => {
   const { toggleColorMode } = useContext(ThemeContext);
@@ -19,6 +22,9 @@ export const NavBar = () => {
   const theme = useTheme();
 
   const mode = theme.palette.mode;
+
+  const dispatch = useDispatch();
+  const session = useSelector((state: any) => state.auth.session);
 
   return (
     <>
@@ -34,7 +40,7 @@ export const NavBar = () => {
           color: '#000',
           boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)',
           top: 0,
-            left: 0,
+          left: 0,
           zIndex: (theme) => theme.zIndex.drawer + 10,
         }}
       >
@@ -54,9 +60,18 @@ export const NavBar = () => {
               aria-label="toggle-dark-mode"
             />
             {/* Testing Theme font. */}
-            <Typography variant='h1' boxShadow={theme.shadows[2]}>
-                Illusia Ry Storage Solutions(Font Test)
+            <Typography variant="h1" boxShadow={theme.shadows[2]}>
+              Illusia Ry Storage Solutions(Font Test)
             </Typography>
+            {session ? (
+              <Button onClick={() => dispatch(logout())} color="inherit">
+                Logout
+              </Button>
+            ) : (
+              <Button href="/login" color="inherit">
+                Login
+              </Button>
+            )}
           </Box>
         </Toolbar>
       </AppBar>
