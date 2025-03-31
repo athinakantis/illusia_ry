0047667a331451273f882ab9b936e20eff88ca23
Athina <athina.kantis@gmail.com>
2025-03-31 13:18:54 +0300

    Add Items route

--- frontend/src/router/router.tsx
+++ frontend/src/router/router.tsx
@@ -1,9 +1,13 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Home from '../pages/Home';
+import ItemsPage from '../pages/Items';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <Home />,
+  }, {
+    path: '/items',
+    element: <ItemsPage />
   }
 ])
