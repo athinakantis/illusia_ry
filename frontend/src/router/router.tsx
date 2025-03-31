45b80335c16cc249280c2d6081427495c2b427d9
Andrei Savolainen <andrei.savolainen@gmail.com>
2025-03-31 18:57:00 +0300

    Use useSelector to protect routes based on the Redux auth state.

--- frontend/src/router/router.tsx
+++ frontend/src/router/router.tsx
@@ -1,9 +1,23 @@
 import { createBrowserRouter } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import Home from '../pages/Home';
+import ProtectedPage from '../pages/ProtectedPage';
+
+const ProtectedRoute = ({ element }: { element: JSX.Element }) => {
+  const session = useSelector((state: any) => state.auth.session);
+
+  if (!session) return <div>Access Denied</div>;
+
+  return element;
+};
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <Home />,
-  }
-])
+  },
+  {
+    path: '/protected',
+    element: <ProtectedRoute element={<ProtectedPage />} />,
+  },
+]);
