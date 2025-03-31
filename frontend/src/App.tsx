9f62eeef01c24aada575b4042d95dd5b61676467
Athina <athina.kantis@gmail.com>
2025-03-31 17:33:48 +0300

    Add Store Provider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,16 @@ import { RouterProvider } from 'react-router-dom';
 import { router } from './router/router';
 import './App.css';
 import { NavBar } from './components/NavBar';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 
 function App() {
   return (
     <Box>
       <NavBar />
-      <RouterProvider router={router} />
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
     </Box>
   );
 }
