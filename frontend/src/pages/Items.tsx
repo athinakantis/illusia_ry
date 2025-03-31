10300f22f9fd00f826ba53741f955b393ddfaefc
Athina <athina.kantis@gmail.com>
2025-03-31 13:19:03 +0300

    Add items page

---
+++ frontend/src/pages/Items.tsx
@@ -0,0 +1,13 @@
+import UserItems from '../components/Items';
+import AdminItems from '../components/Admin/Items';
+
+function Items() {
+  return (
+    <>
+      <AdminItems />
+      <UserItems />
+    </>
+  )
+}
+
+export default Items;
