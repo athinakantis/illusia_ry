d26dcb2a44f271a504fb1b0db130a4cb9175bb35
Athina <athina.kantis@gmail.com>
2025-03-31 17:34:16 +0300

    Add initial items component

--- frontend/src/components/Items.tsx
+++ frontend/src/components/Items.tsx
@@ -1,11 +1,26 @@
 import { useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from '../store/hooks';
+import { fetchAllItems, selectAllItems } from '../slices/itemsSlice';
+
 
 function Items() {
+  const items = useAppSelector(selectAllItems)
+  const dispatch = useAppDispatch()
+
   useEffect(() => {
+    if (items.length < 1) {
+      dispatch(fetchAllItems())
+    }
+  }, [dispatch, items])
 
-  }, [])
-
-  return <>Items component here</>
+  return (
+    <>
+      {items.map(item => (
+        <p>{item.item_name}</p>
+      )
+      )}
+    </>
+  )
 }
 
 export default Items;
