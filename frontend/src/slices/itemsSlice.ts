691ef4b14dad18bb44f9475ad6ae1df65bf0bc69
Athina <athina.kantis@gmail.com>
2025-03-31 13:42:22 +0300

    add initial Items slice

---
+++ frontend/src/slices/itemsSlice.ts
@@ -0,0 +1,40 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { ItemState } from '../types/types';
+import { itemsApi } from '../api/items';
+import { RootState } from '../store/store';
+
+const initialState: ItemState = {
+  items: [],
+  loading: false,
+  error: null
+}
+
+export const fetchAllItems = createAsyncThunk(
+  'items/fetchAllItems',
+  async () => {
+    const response = await itemsApi.getAllItems();
+    return response;
+  }
+);
+
+
+export const itemsSlice = createSlice({
+  name: 'items',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchAllItems.pending, (state) => {
+      state.loading = true
+    })
+    builder.addCase(fetchAllItems.fulfilled, (state) => {
+      state.loading = true
+    })
+    builder.addCase(fetchAllItems.rejected, (state) => {
+      state.loading = true
+    })
+  }
+})
+
+export const selectAllItems = (state: RootState) =>
+  state.items;
+export default itemsSlice.reducer;
