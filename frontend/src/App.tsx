1a3c186fb03b819aef3e42e97785f2fe910c3868
Jonathan Gian <153126165+JonathanGian@users.noreply.github.com>
2025-04-08 09:36:23 +0300

    Remove CSS

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import { Box } from '@mui/material';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router/router';
-import './App.css';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
 
