0044c1352d84be6ebceb758bd0cac5b9df21ebad
Athina <athina.kantis@gmail.com>
2025-04-08 10:05:31 +0300

    Add home page to router

--- frontend/src/router/router.tsx
+++ frontend/src/router/router.tsx
@@ -3,12 +3,17 @@ import { AuthRedirect } from '../components/auth/AuthRedirect';
 import LoginPage from '../pages/LoginPage';
 import ItemsPage from '../pages/Items';
 import Root from '../components/Root';
+import Home from '../pages/Home';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
     children: [
+      {
+        path: '/',
+        element: <Home />
+      },
       {
         path: '/login',
         element: (
