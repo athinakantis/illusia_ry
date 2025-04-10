9cf7cf9000e548fc578d04a3dc36d84f21760ee2
Athina <athina.kantis@gmail.com>
2025-04-10 20:18:14 +0300

    Add pagination to UserItems

--- frontend/src/components/Items.tsx
+++ frontend/src/components/Items.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../store/hooks';
 import { fetchAllItems, selectAllItems } from '../slices/itemsSlice';
 import {
@@ -13,30 +13,34 @@ import {
   TextField,
 } from '@mui/material';
 import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
-
-
+import Pagination from './Pagination';
 
 function Items() {
   const items = useAppSelector(selectAllItems);
   const dispatch = useAppDispatch();
+  const [offset, setOffset] = useState(0)
+
 
   useEffect(() => {
     if (items.length < 1) {
-      dispatch(fetchAllItems());
+      dispatch(fetchAllItems())
     }
-  }, [dispatch, items]);
+  }, [dispatch, items])
 
   return (
     <Box
       sx={{
         width: '95%',
         margin: 'auto',
-        display: 'flex'
-      }}>
+        display: 'flex',
+        pb: '8rem'
+      }}
+    >
       <Box
         sx={{
-          minWidth: 300
-        }}>
+          minWidth: 300,
+        }}
+      >
         <TextField
           id="filled-search"
           label="Search our items"
@@ -45,57 +49,65 @@ function Items() {
           sx={{ width: '80%', mt: 1 }}
         />
       </Box>
-      <Stack
-        direction={'row'}
-        flexWrap={'wrap'}
-        gap={3}
-        justifyContent={'center'}
-        maxWidth={'95%'}
-        justifySelf={'center'}
-        padding={'none'}
-        textAlign={'start'}
-      >
-        {items.map((item) => (
-          <Card
-            key={item.item_id}
-            sx={{ width: 280, minHeight: 300, boxShadow: 'none' }}
-          >
-            <CardMedia
-              sx={{
-                bgcolor: 'background.lightgrey',
-                height: '300px',
-                borderRadius: '14px',
-              }}
-              src={
-                item.image_path ||
-                'https://placehold.co/200x200?text=hello+world'
-              }
-            />
-            <CardContent
-              sx={{
-                padding: '0.5rem 0',
-                mb: '1rem',
-                display: 'flex',
-                justifyContent: 'space-between',
-              }}
-            >
-              <Box sx={{ width: '80%', alignItems: 'center', display: 'flex' }}>
-                <Typography variant="body1" sx={{ width: '70%' }}>
-                  {item.item_name}
-                </Typography>
-              </Box>
 
-              <CardActions
-                sx={{ padding: 0, justifySelf: 'end', width: 'fit-content' }}
+
+      <Box
+        sx={{
+          display: 'flex', flexDirection: 'column', flex: 1,
+          '& .css-1c5o7vy-MuiPagination-ul': { justifyContent: 'center', pt: 2 }
+        }}>
+        <Stack
+          direction={'row'}
+          flexWrap={'wrap'}
+          gap={3}
+          justifySelf={'center'}
+          padding={'none'}
+          textAlign={'start'}
+        >
+
+          {items.slice(offset, offset + 8).map((item) => (
+            <Card
+              key={item.item_id}
+              sx={{ width: 280, minHeight: 300, boxShadow: 'none' }}
+            >
+              <CardMedia
+                sx={{
+                  bgcolor: 'background.lightgrey',
+                  height: '300px',
+                  borderRadius: '14px',
+                }}
+                src={
+                  item.image_path ||
+                  'https://placehold.co/200x200?text=hello+world'
+                }
+              />
+              <CardContent
+                sx={{
+                  padding: '0.5rem 0',
+                  mb: '1rem',
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                }}
               >
-                <Button sx={{ padding: '3px', minWidth: 'fit-content' }}>
-                  <AddCircleOutlineOutlinedIcon />
-                </Button>
-              </CardActions>
-            </CardContent>
-          </Card>
-        ))}
-      </Stack>
+                <Box sx={{ width: '80%', alignItems: 'center', display: 'flex' }}>
+                  <Typography variant="body1" sx={{ width: '70%' }}>
+                    {item.item_name}
+                  </Typography>
+                </Box>
+
+                <CardActions
+                  sx={{ padding: 0, justifySelf: 'end', width: 'fit-content' }}
+                >
+                  <Button sx={{ padding: '3px', minWidth: 'fit-content' }}>
+                    <AddCircleOutlineOutlinedIcon />
+                  </Button>
+                </CardActions>
+              </CardContent>
+            </Card>
+          ))}
+        </Stack>
+        <Pagination items={items} setOffset={setOffset} />
+      </Box>
     </Box>
   );
 }
