db6e4046fae01a1b8c29af90347e8376ffbe58da
Jonathan Gian <153126165+JonathanGian@users.noreply.github.com>
2025-04-21 10:48:44 +0300

    Added a utility to get a fresh token for use with Postman

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,9 @@
+import { getAccessToken } from "../utility/getToken";
 
 function Home() {
 
-  return <div>This is the Home route</div>
-}
+  return(
+   <div>This is the Home route<button onClick={getAccessToken}>token</button></div>
+)}
 
 export default Home;
