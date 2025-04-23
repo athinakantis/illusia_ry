8ec22898c80239f651efa506cd3e526a2819b033
Athina <athina.kantis@gmail.com>
2025-04-23 20:24:50 +0300

    Update cart UI

--- frontend/src/pages/Cart.tsx
+++ frontend/src/pages/Cart.tsx
@@ -1,9 +1,24 @@
-import { Box, Button } from '@mui/material';
+import {
+    Box,
+    Button,
+    Paper,
+    Stack,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Typography,
+} from '@mui/material';
 import { useAppDispatch, useAppSelector } from '../store/hooks';
-import { emptyCart, loadCartFromStorage, removeItemFromCart, selectCart } from '../slices/cartSlice';
-import { DataGridGeneric } from '../components/CustomComponents/DataGridGeneric';
-import { selectAllItems } from '../slices/itemsSlice';
-import ClearIcon from '@mui/icons-material/Clear';
+import {
+    emptyCart,
+    loadCartFromStorage,
+    removeItemFromCart,
+    selectCart,
+} from '../slices/cartSlice';
+import { fetchAllItems, selectAllItems } from '../slices/itemsSlice';
 import { addBooking } from '../slices/bookingsSlice';
 import { useAuth } from '../hooks/useAuth';
 import { showNotification } from '../slices/notificationSlice';
@@ -18,31 +33,28 @@ function Cart() {
 
     // Load locally stored cart
     useEffect(() => {
-        const savedCart: LocalReservation[] = JSON.parse(localStorage.getItem('savedCart') ?? '[]')
+        if (items.length <= 1) dispatch(fetchAllItems())
+        const savedCart: LocalReservation[] = JSON.parse(
+            localStorage.getItem('savedCart') ?? '[]',
+        );
         // If cart has less items than the locally stored array
         // load from storage
         if (cart.length < savedCart.length) {
-            dispatch(loadCartFromStorage(savedCart))
+            dispatch(loadCartFromStorage(savedCart));
         }
-    }, [])
-
-    const cartInfo = cart.map((itemToShow) => ({
-        ...items.find((item) => item.item_id === itemToShow.item_id),
-        ...itemToShow,
-    }));
+    }, []);
 
 
-    const usedColumns = [
-        { columnName: 'Item ID', columnField: 'item_id' },
-        { columnName: 'Item Name', columnField: 'item_name' },
-        { columnName: 'Start Date', columnField: 'start_date' },
-        { columnName: 'End Date', columnField: 'end_date' },
-        { columnName: 'Pcs ordered', columnField: 'quantity' },
-    ];
+    const mockItems = items.slice(0, 3)
+
 
     const createBookingFromCart = () => {
         return { user_id: user?.id, items: cart };
     };
+
+    const handleBrokenImg = (e: React.SyntheticEvent<HTMLImageElement, Event>) => {
+        (e.target as HTMLImageElement).src = '/src/assets/broken_img.png';
+    }
 
     const handleAddBooking = async () => {
         const newBookingData: object = createBookingFromCart();
@@ -66,54 +78,92 @@ function Cart() {
     };
 
     return (
-        <>
-            <Box
-                sx={{
-                    mt: 5,
-                    width: '100%',
-                    display: 'flex',
-                    justifyContent: 'center',
-                    padding: 4,
-                    boxSizing: 'border-box',
-                }}
-            >
-                {cart.length > 0 ? (
-                    <>
-                        <DataGridGeneric
-                            data={cartInfo}
-                            idColumn={'item_id'}
-                            usedColumns={usedColumns}
-                            functions={[
-                                {
-                                    functionName: 'remove',
-                                    functionIcon: <ClearIcon />,
-                                    functionBody: (
-                                        item_id: string,
-                                        quantityToRemove: number = 1,
-                                    ) => {
-                                        dispatch(removeItemFromCart({ item_id, quantityToRemove }));
-                                    },
-                                },
-                            ]}
-                        />
-                    </>
-                ) : (
-                    <p>Cart is empty</p>
-                )}
-            </Box>
-            <Button variant="contained" color="primary" onClick={handleAddBooking}>
-                {' '}
-                Make booking
-            </Button>
-            <Button
-                variant="contained"
-                color="primary"
-                onClick={() => dispatch(emptyCart())}
-            >
-                {' '}
-                Clear cart
-            </Button>
-        </>
+        <Box
+            sx={{
+                maxWidth: 1240,
+                m: '0 auto',
+            }}
+        >
+            <Typography variant="heading_secondary_bold">Your Cart</Typography>
+
+            {cart.length > 0 ? (
+                <Stack direction={'row'}
+                    sx={{
+                        gap: '32px',
+                        flexWrap: 'wrap'
+                    }}
+                >
+                    <TableContainer
+                        sx={{ maxWidth: 816 }}>
+                        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Items ({cart.length})</TableCell>
+                                    <TableCell align="right">Qty</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {mockItems.map((item) => (
+                                    <TableRow
+                                        key={item.item_id}
+                                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                    >
+                                        <TableCell>
+                                            <Stack direction={'row'} sx={{ gap: '21px' }}>
+                                                <img src={item.image_path} style={{ width: 78, borderRadius: 14 }}
+                                                    onError={handleBrokenImg} />
+                                                <Stack sx={{ maxWidth: 186 }}>
+                                                    <Typography>{item.item_name}</Typography>
+                                                </Stack>
+                                            </Stack>
+                                        </TableCell>
+                                        <TableCell align="right">
+                                            <Typography>1</Typography>
+                                        </TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+
+                    {/* Booking Summary */}
+                    <Stack id="booking_summary"
+                        sx={{
+                            border: '1px solid #E2E2E2',
+                            padding: '40px 30px',
+                            maxWidth: 392,
+                            flex: 1,
+                            gap: '24px',
+                            height: 'fit-content'
+                        }}>
+                        <Typography
+                            variant="body3"
+                            sx={{
+                                fontSize: 22,
+                                borderLeft: '2px solid black',
+                                color: 'text.primary',
+                                pl: 2,
+                                fontWeight: 500
+                            }}
+                        >
+                            Booking Summary
+                        </Typography>
+                        <Stack direction={'row'} justifyContent={'space-between'}>
+                            <Typography variant='body2'>Dates</Typography>
+                            <Typography variant='body2'>2024-04-19 - 2024-04-22</Typography>
+                        </Stack>
+                        <Stack direction={'row'} justifyContent={'space-between'}>
+                            <Typography variant='body2'>Total items</Typography>
+                            <Typography variant='body2'>{cart.length}</Typography>
+                        </Stack>
+                        <Button variant='rounded' size='small' onClick={handleAddBooking}>Book items</Button>
+                    </Stack>
+
+                </Stack>
+            ) : (
+                <></>
+            )}
+        </Box>
     );
 }
 
