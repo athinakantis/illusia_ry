f9c245eea5699622c96e3c0d65f388d9b7fa7688
Athina <athina.kantis@gmail.com>
2025-04-23 08:02:11 +0300

    load items from localstorage upon mount

--- frontend/src/pages/Cart.tsx
+++ frontend/src/pages/Cart.tsx
@@ -1,58 +1,69 @@
-import { Box, Button } from "@mui/material";
-import { useAppDispatch, useAppSelector } from "../store/hooks";
-import { emptyCart, removeItemFromCart, selectCart } from "../slices/cartSlice";
+import { Box, Button } from '@mui/material';
+import { useAppDispatch, useAppSelector } from '../store/hooks';
+import { emptyCart, loadCartFromStorage, removeItemFromCart, selectCart } from '../slices/cartSlice';
 import { DataGridGeneric } from '../components/CustomComponents/DataGridGeneric';
-import { selectAllItems } from "../slices/itemsSlice";
+import { selectAllItems } from '../slices/itemsSlice';
 import ClearIcon from '@mui/icons-material/Clear';
-import { addBooking } from "../slices/bookingsSlice";
-import { useAuth } from "../hooks/useAuth";
-import { showNotification } from "../slices/notificationSlice";
-
+import { addBooking } from '../slices/bookingsSlice';
+import { useAuth } from '../hooks/useAuth';
+import { showNotification } from '../slices/notificationSlice';
+import { LocalReservation } from '../types/types';
+import { useEffect } from 'react';
 
 function Cart() {
-
     const dispatch = useAppDispatch();
-    const itemsInCart = useAppSelector(selectCart);
+    const cart = useAppSelector(selectCart);
     const items = useAppSelector(selectAllItems);
     const { user } = useAuth();
 
-    const itemsInCartInfo = itemsInCart.map(itemToShow => ({
-        ...items.find(item => item.item_id === itemToShow.item_id),
+    // Load locally stored cart
+    useEffect(() => {
+        const savedCart: LocalReservation[] = JSON.parse(localStorage.getItem('savedCart') ?? '[]')
+        // If cart has less items than the locally stored array
+        // load from storage
+        if (cart.length < savedCart.length) {
+            dispatch(loadCartFromStorage(savedCart))
+        }
+    }, [])
+
+    const cartInfo = cart.map((itemToShow) => ({
+        ...items.find((item) => item.item_id === itemToShow.item_id),
         ...itemToShow,
     }));
 
+
     const usedColumns = [
-        { columnName: "Item ID", columnField: "item_id" },
-        { columnName: "Item Name", columnField: "item_name" },
-        { columnName: "Start Date", columnField: "start_date" },
-        { columnName: "End Date", columnField: "end_date" },
-        { columnName: "Pcs ordered", columnField: "quantity" },
+        { columnName: 'Item ID', columnField: 'item_id' },
+        { columnName: 'Item Name', columnField: 'item_name' },
+        { columnName: 'Start Date', columnField: 'start_date' },
+        { columnName: 'End Date', columnField: 'end_date' },
+        { columnName: 'Pcs ordered', columnField: 'quantity' },
     ];
 
     const createBookingFromCart = () => {
-        return { user_id: user?.id, items: itemsInCart };
-    }
-
+        return { user_id: user?.id, items: cart };
+    };
 
     const handleAddBooking = async () => {
         const newBookingData: object = createBookingFromCart();
         const resultAction = await dispatch(addBooking(newBookingData));
 
         if (addBooking.rejected.match(resultAction)) {
-            dispatch(showNotification({
-                message: resultAction.payload ?? "unknown error",
-                severity: 'error',
-            }));
+            dispatch(
+                showNotification({
+                    message: resultAction.payload ?? 'unknown error',
+                    severity: 'error',
+                }),
+            );
         } else {
-            dispatch(showNotification({
-                message: 'Booking created',
-                severity: 'success',
-            }));
-
+            dispatch(
+                showNotification({
+                    message: 'Booking created',
+                    severity: 'success',
+                }),
+            );
         }
-
-
-    }
+    };
 
     return (
         <>
@@ -66,36 +77,44 @@ function Cart() {
                     boxSizing: 'border-box',
                 }}
             >
-                {itemsInCart.length > 0 ? (
+                {cart.length > 0 ? (
                     <>
-                        <DataGridGeneric data={itemsInCartInfo} idColumn={"item_id"} usedColumns={usedColumns} functions={[
-                            {
-                                functionName: "remove", functionIcon: <ClearIcon />, functionBody: (item_id: string, quantityToRemove: number = 1) => {
-                                    dispatch(removeItemFromCart({ item_id, quantityToRemove }));
-                                }
-                            },
-                        ]} />
-
+                        <DataGridGeneric
+                            data={cartInfo}
+                            idColumn={'item_id'}
+                            usedColumns={usedColumns}
+                            functions={[
+                                {
+                                    functionName: 'remove',
+                                    functionIcon: <ClearIcon />,
+                                    functionBody: (
+                                        item_id: string,
+                                        quantityToRemove: number = 1,
+                                    ) => {
+                                        dispatch(removeItemFromCart({ item_id, quantityToRemove }));
+                                    },
+                                },
+                            ]}
+                        />
                     </>
                 ) : (
                     <p>Cart is empty</p>
-                )
-                }
-
-            </Box >
-            <Button
-                variant="contained"
-                color="primary"
-                onClick={handleAddBooking}
-            > Make booking</Button>
+                )}
+            </Box>
+            <Button variant="contained" color="primary" onClick={handleAddBooking}>
+                {' '}
+                Make booking
+            </Button>
             <Button
                 variant="contained"
                 color="primary"
                 onClick={() => dispatch(emptyCart())}
-            > Clear cart</Button>
+            >
+                {' '}
+                Clear cart
+            </Button>
         </>
     );
-
 }
 
 export default Cart;
--- frontend/src/pages/Items.tsx
+++ frontend/src/pages/Items.tsx
@@ -1,12 +1,32 @@
 import UserItems from '../components/Items';
 import AdminItems from '../components/Admin/Items'
 import { useAuth } from '../hooks/useAuth';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { FormControlLabel, Switch } from '@mui/material';
+import { useDispatch } from 'react-redux';
+import { loadCartFromStorage, selectCart } from '../slices/cartSlice';
+import { useAppSelector } from '../store/hooks';
 
 function Items() {
   const { role } = useAuth()
   const [view, setView] = useState<boolean>(false)
+  const dispatch = useDispatch()
+  const cart = useAppSelector(selectCart)
+
+  type LocallyStoredItem = {
+    item_id: string;
+    quantity: number
+  }
+
+  // Load locally stored cart
+  useEffect(() => {
+    const savedCart: LocallyStoredItem[] = JSON.parse(localStorage.getItem('savedCart') ?? '[]')
+    // If cart has less items than the locally stored array
+    // load from storage
+    if (cart.length < savedCart.length) {
+      dispatch(loadCartFromStorage(savedCart))
+    }
+  }, [])
 
 
   // If user is Admin or Head Admin, return AdminItems / UserItems
@@ -17,7 +37,7 @@ function Items() {
           marginLeft: '20px',
           color: 'primary.main',
           '& label, span': { fontFamily: 'Roboto Slab, sans-serif' }
-        }} // Add custom Typography type?
+        }}
         control={<Switch onChange={() => setView(prev => !prev)} />}
         label={`View as ${view ? 'User' : 'Admin'}`} />
       {view ? <UserItems /> : <AdminItems />}
