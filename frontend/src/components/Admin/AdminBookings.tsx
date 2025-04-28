8320231e3089588abf99d93af93d9e9293efea5c
Jonathan Gian <153126165+JonathanGian@users.noreply.github.com>
2025-04-28 15:51:40 +0300

    Added AdminBookings.tsx and minor updates to AdminDashboard

---
+++ frontend/src/components/Admin/AdminBookings.tsx
@@ -0,0 +1,10 @@
+const AdminBookings = () => {
+    return (
+        <div>
+        <h1>Admin Bookings</h1>
+        <p>Manage bookings here.</p>
+        </div>
+    );
+}
+
+export default AdminBookings;
