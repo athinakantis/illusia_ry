d7ff7fb05b7893f909b3e0a1ea73393b398115b6
Athina <athina.kantis@gmail.com>
2025-04-28 20:45:01 +0300

    Remove unused imports

--- frontend/src/components/Admin/AdminAddProduct.tsx
+++ frontend/src/components/Admin/AdminAddProduct.tsx
@@ -5,7 +5,6 @@ import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../../hooks/useAuth'; // Import your auth hook
 import {
     createItem,
-    fetchAllCategories,
     selectAllCategories,
 } from '../../slices/itemsSlice';
 import { supabase } from '../../config/supabase';
