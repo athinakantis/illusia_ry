9ab6b3e7f8be9a8043c99c5d2b5b1acdefa8e201
Athina <athina.kantis@gmail.com>
2025-04-29 20:01:32 +0300

    Add renderHTML function, update sendMail function to take optional HTML and attachment properties

--- backend/src/services/mailer.service.ts
+++ backend/src/services/mailer.service.ts
@@ -1,19 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { google } from 'googleapis';
-import  { createTransport, SentMessageInfo } from 'nodemailer';
+import { createTransport, SentMessageInfo } from 'nodemailer';
 // import hbs from 'nodemailer-express-handlebars';
+import { promises as fs } from 'fs';
+import * as path from 'path';
+
 
 
 @Injectable()
 export class MailerService {
-  private transporter
+  private transporter;
 
   constructor(private config: ConfigService) {
-    const clientId     = this.config.get<string>('GOOGLE_WEB_CLIENT_ID'); // Client ID from Google Cloud Console
+    const clientId = this.config.get<string>('GOOGLE_WEB_CLIENT_ID'); // Client ID from Google Cloud Console
     const clientSecret = this.config.get<string>('GOOGLE_WEB_SECRET'); // Client Secret from Google Cloud Console
     const refreshToken = this.config.get<string>('GOOGLE_WEB_REFRESH_TOKEN'); // Refresh Token from Google OAuth2 Playground
-    const userEmail    = this.config.get<string>('EMAIL'); // User email that will send the email
+    const userEmail = this.config.get<string>('EMAIL'); // User email that will send the email
 
     const oAuth2Client = new google.auth.OAuth2(
       clientId,
@@ -35,7 +38,6 @@ export class MailerService {
 
     // patch to auto-refresh the access token
     const orig = this.transporter.sendMail.bind(this.transporter);
-    
 
     this.transporter.sendMail = async (options) => {
       const { token } = await oAuth2Client.getAccessToken();
@@ -44,9 +46,17 @@ export class MailerService {
       return orig(options);
     };
   }
-  
-  
 
+  
+  /**
+   * Render HTML function that returns the page
+   * to be rendered in an email
+   */
+  async renderHtml(page: string) {
+    const htmlPath = path.join(__dirname, '..', 'email_content', `${page}.html`)
+    const htmlFromPage = await fs.readFile(htmlPath, 'utf-8');
+    return htmlFromPage
+  }
 
   /**
    * Low‐level send helper
@@ -54,13 +64,21 @@ export class MailerService {
   async sendEmail(
     to: string,
     subject: string,
-    text: string
+    text: string,
+    html?: string,
+    attachments?: {
+      fileName: string,
+      cid: string,
+      path: string
+    }[]
   ): Promise<SentMessageInfo> {
     return this.transporter.sendMail({
       from: this.config.get<string>('EMAIL'),
       to,
       subject,
       text,
+      html,
+      attachments
     });
   }
 
@@ -69,18 +87,24 @@ export class MailerService {
    * @param to           The user’s email address.
    * @param displayName  The user’s display name.
    */
-  async sendSignupEmail(
-    to: string,
-    displayName: string
-  ) {
+  async sendSignupEmail(to: string, displayName: string) {
     const subject = 'Welcome to Illusia!';
     const text = `Hi ${displayName},
 
-Thank you for signing up for Illusia. We're excited to have you onboard!
+    Thank you for signing up for Illusia. We're excited to have you onboard!
 
-Best regards,
-The Illusia Team`;
-    return this.sendEmail(to, subject, text);
+    Best regards,
+    The Illusia Team`;
+
+    const PAGE = 'signup'
+    const html = await this.renderHtml(PAGE) 
+
+    const attachments = [{
+      fileName: "welcome.jpg",
+      path: "https://crralkzqnflfzntlhccj.supabase.co/storage/v1/object/public/email-images//41103945280_6ba926b4bc_k_resized.jpg",
+      cid: "illusia_ry_signup_notification"
+    }]
+    return this.sendEmail(to, subject, text, html, attachments);
   }
 
   /**
@@ -94,7 +118,7 @@ The Illusia Team`;
     to: string,
     bookingId: string,
     startDate?: string,
-    endDate?: string
+    endDate?: string,
   ) {
     const subject = 'Your booking has been approved';
     let text = `Hello,
@@ -111,6 +135,8 @@ You can view more details in your dashboard.
 
 Thanks,
 The Illusia Team`;
-    return this.sendEmail(to, subject, text);
+    const html = `
+`;
+    return this.sendEmail(to, subject, text, html);
   }
 }
