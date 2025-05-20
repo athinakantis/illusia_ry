50850aab9fe2a994f27889a4b40cb88ede734062
Jonathan Gian <153126165+JonathanGian@users.noreply.github.com>
2025-05-20 12:53:05 +0300

    Added tags api

--- frontend/src/api/tags.ts
+++ frontend/src/api/tags.ts
@@ -2,5 +2,9 @@ import { ApiResponse, ItemState } from '../types/types';
 import { api } from './axios';
 
 export const tagsApi = {
-  getAllTags: (): Promise<ApiResponse<ItemState['tags']>> => api.get('items/tags')
+  getAllTags: (): Promise<ApiResponse<ItemState['tags']>> => api.get('items/tags'),
+
+  getAllItemTags: (): Promise<ApiResponse<ItemState['item_tags']>> => api.get('items/item_tags'),
+
+  findByItem: (itemId: string): Promise<ApiResponse<ItemState['tags']>> => api.get(`items/${itemId}/tags`),
 }
