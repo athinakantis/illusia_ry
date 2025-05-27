74e3d96f0a2b5b19131c351584457cf9b5be796e
JonathanGian <jongian90@gmail.com>
2025-05-27 13:05:25 +0300

    Applied middleware to only some endpoints in reservations controller

--- backend/src/modules/reservations/reservations.module.ts
+++ backend/src/modules/reservations/reservations.module.ts
@@ -1,8 +1,9 @@
 import { SupabaseModule } from '../supabase/supabae.module';
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { ItemReservationsController } from './reservations.controller';
 import { ItemReservationService } from './reservations.service';
 import { GuestModule } from '../guest/guest.module';
+import { AuthMiddleware } from 'src/middlewares/auth.middleware';
 
 @Module({
   imports: [SupabaseModule,GuestModule],
@@ -10,4 +11,15 @@ import { GuestModule } from '../guest/guest.module';
   providers:    [ItemReservationService],
   exports:      [],   // only export the service if other modules need it
 })
-export class ItemReservationsModule {}
+export class ItemReservationsModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AuthMiddleware)
+      // Only apply to some endpoints
+      .forRoutes(
+        { path: 'reservations/', method: RequestMethod.POST },
+        { path: 'reservations/booking/:bookingId', method: RequestMethod.DELETE },
+        { path: "reservations/booking/:bookingId/reservationId", method: RequestMethod.PATCH }
+      );
+  }
+}
