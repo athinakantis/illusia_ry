a2014cfd36063d7b3dc614e7c5d5648efc646758
Athina <athina.kantis@gmail.com>
2025-05-29 10:11:27 +0300

    Added translationKeys to error messages

--- frontend/src/selectors/availabilitySelector.ts
+++ frontend/src/selectors/availabilitySelector.ts
@@ -27,23 +27,22 @@ export const checkAvailabilityForItemOnDates = (
             const availableQuantityOfItem = overallItemQty - itemQtyInReservatios - (includeCart ? itemQtyInCart : 0);
 
             if ((includeCart) && (overallItemQty === itemQtyInCart))
-                return { severity: 'warning', message: 'Max quantity of the item is already in cart' };
+                return { severity: 'warning', message: 'Max quantity of the item is already in cart', translationKey: 'cart.snackbar.maxQtyError' };
 
             if (includeCart && availableQuantityOfItem === 0) {
-                if (itemQtyInCart === 0) return { severity: 'warning', message: "Item is not available for selected dates" };
-                else return { severity: 'warning', message: "Max quantity of the item for the dates is already in cart" };
-
+                if (itemQtyInCart === 0) return { severity: 'warning', message: "Item is not available for selected dates", translationKey: 'cart.snackbar.datesUnavailable' };
+                else return { severity: 'warning', message: "Max quantity of the item for the dates is already in cart", translationKey: 'cart.snackbar.maxQtyDateError' };
             }
 
             if (overallItemQty < quantity)
-                return { severity: 'warning', message: `Requested bigger quantity than available overall by ${quantity - overallItemQty}` };
+                return { severity: 'warning', message: `Requested bigger quantity than available overall by ${quantity - overallItemQty}`, translationKey: 'cart.snackbar.qtyUnavailable' };
 
             return quantity - availableQuantityOfItem <= 0
                 ? { severity: 'success', data: true }
                 : {
                     severity: 'warning',
-                    // message: `Not enough of item available for selected dates. Only available ${overallItemQty - itemQtyInReservatios}`,
                     message: `Not enough of item available for selected dates.`,
+                    translationKey: 'cart.snackbar.itemQtyUnavailable'
                 };
         },
 
